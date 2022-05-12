ad4e1c74c12c38529312a862901981c033a795ee
gift56 <efeasiughu@gmail.com>
2022-05-12 19:50:50 +0100

    locating item with for loop

--- myjavscript/loop.js
+++ myjavscript/loop.js
@@ -64,3 +64,42 @@ function sum(arr, n) {
         return sum(arr, n - 1) + arr[n - 1];
     }
 }
+// Returning object with for loop
+const contacts = [
+    {
+        firstName: "Akira",
+        lastName: "Laine",
+        number: "0543236543",
+        likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+        firstName: "Harry",
+        lastName: "Potter",
+        number: "0994372684",
+        likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+        firstName: "Sherlock",
+        lastName: "Holmes",
+        number: "0487345643",
+        likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+        firstName: "Kristian",
+        lastName: "Vos",
+        number: "unknown",
+        likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+];
+
+function lookUpProfile(name, prop) {
+
+    for (let i = 0; i < contacts.length; i++) {
+        if (contacts[i].firstName === name) {
+            return contacts[i][prop] || "No such property"
+        }
+    }
+    return "No such contact";
+}
+
+lookUpProfile("Akira", "likes");
