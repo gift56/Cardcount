c8df4a9c5cca432ec9d24638c0684b6d5c994eee
gift56 <efeasiughu@gmail.com>
2022-05-12 15:04:12 +0100

    changing player code with varible assign

--- myjavscript/person.js
+++ myjavscript/person.js
@@ -7,3 +7,13 @@ const person = {
 }
 
 console.log(person);
+
+const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+};
+
+// assign playername to player
+const playerNumber = 16;
+const player = testObj[playerNumber];
