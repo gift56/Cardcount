1a02f2294e1610544c06fba18c613c5a1f786fb9
gift56 <efeasiughu@gmail.com>
2022-05-12 17:00:48 +0100

    updated javascript props

--- myjavscript/person.js
+++ myjavscript/person.js
@@ -88,3 +88,41 @@ const myPlants = [
     }
 ];
 const secondTree = myPlants[1].list[1];
+
+// creating prob value and id withjavascript object
+
+const recordCollection = {
+    2548: {
+        albumTitle: 'Slippery When Wet',
+        artist: 'Bon Jovi',
+        tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+        albumTitle: '1999',
+        artist: 'Prince',
+        tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+        artist: 'Robert Palmer',
+        tracks: []
+    },
+    5439: {
+        albumTitle: 'ABBA Gold'
+    }
+};
+
+function updateRecords(records, id, prop, value) {
+    if (value === '') {
+        delete records[id][prop];
+    }
+    else if (prop === 'tracks') {
+        records[id][prop] = records[id][prop] || [];
+        records[id][prop].push(value);
+    }
+    else {
+        records[id][prop] = value;
+    }
+    return records;
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
