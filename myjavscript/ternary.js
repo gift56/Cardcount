321000d08f7a30f4aaf864a66eeac12f4c89723a
gift56 <efeasiughu@gmail.com>
2022-05-12 20:38:21 +0100

    multiple statement

--- myjavscript/ternary.js
+++ myjavscript/ternary.js
@@ -1,5 +1,11 @@
 function checkEqual(a, b) {
     return a == b ? "Equal" : "Not Equal";
 }
-
 checkEqual(1, 2);
+
+// multiple if else if and else
+function checkSign(num) {
+    return num > 0 ? "positive" : num < 0 ? "negative" : "zero";
+}
+
+checkSign(10);
