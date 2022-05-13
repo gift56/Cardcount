ce1d8a9da16b0cd143b8e55b129e57aa003a3c71
gift56 <efeasiughu@gmail.com>
2022-05-13 17:41:05 +0100

    using arrow function an assigning value

--- myjavscript/es6.js
+++ myjavscript/es6.js
@@ -40,3 +40,5 @@ const magic = () => new Date();
 const myConcat = (arr1, arr2) => arr1.concat(arr2);
 
 console.log(myConcat([1, 2], [3, 4, 5]));
+// assigning value defaultly using arrow function
+const increment = (number, value = 1) => number + value;
