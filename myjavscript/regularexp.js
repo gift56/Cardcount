fb444afbf29f3ac0c9ce1fe39e33d023286f9bc3
gift56 <efeasiughu@gmail.com>
2022-05-14 17:59:20 +0100

    matching all alphabet

--- myjavscript/regularexp.js
+++ myjavscript/regularexp.js
@@ -32,3 +32,8 @@ let resultun = unRegex.test(exampleStr);
 let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
 let vowelRegex = /[aeiou]/gi;
 let resultv = quoteSample.match(vowelRegex); 
+
+// matching all letters
+let quoteSamplems = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let resultMs = quoteSamplems.match(alphabetRegex); // Change this line
