dee2d8f0b7d8d6119f6d69535e8a1198bcc57e44
gift56 <efeasiughu@gmail.com>
2022-05-14 12:29:30 +0100

    destructing of nested object

--- myjavscript/es6.js
+++ myjavscript/es6.js
@@ -64,4 +64,17 @@ const HIGH_TEMPERATURES = {
 
 const { today, tomorrow } = HIGH_TEMPERATURES;
 
-  // Only change code above this line
+// Normal destructuring
+const HIGH_TEMPERATURES_reassignment = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+// destructuring a nested object
+const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+};
+const { today: { low: lowToday, high: highTodays } } = LOCAL_FORECAST;
