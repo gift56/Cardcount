9a1ebc22015c1f825929cce10c86b591cf9d496b
gift56 <efeasiughu@gmail.com>
2022-05-14 11:51:00 +0100

    destructing of object

--- myjavscript/es6.js
+++ myjavscript/es6.js
@@ -53,3 +53,15 @@ let arr2;
 arr2 = [...arr1];
 
 console.log(arr2);
+// destruting of object
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+
+// Only change code below this line
+
+const { today, tomorrow } = HIGH_TEMPERATURES;
+
+  // Only change code above this line
