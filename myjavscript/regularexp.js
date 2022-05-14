0d6461077e5f55508c1abfdf095b39df1773c96d
gift56 <efeasiughu@gmail.com>
2022-05-14 16:57:49 +0100

    using match()

--- myjavscript/regularexp.js
+++ myjavscript/regularexp.js
@@ -2,11 +2,18 @@
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString);
+
 // using the | sign to mmatch multipule objects
 let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/;
 let results = petRegex.test(petString);
+
 // using the i indicaor
 let myStrings = "freeCodeCamp";
 let fccRegex = /freeCodeCamp/i;
 let resulti = fccRegex.test(myString);
+
+// using match()
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/;
+let resultM = extractStr.match(codingRegex);
