dff5d35ef0f77ba9abcf9f326518acf0ae7edf0c
gift56 <efeasiughu@gmail.com>
2022-05-14 12:47:41 +0100

    destructing of array to remove the first movement

--- myjavscript/es6.js
+++ myjavscript/es6.js
@@ -82,3 +82,10 @@ const { today: { low: lowToday, high: highTodays } } = LOCAL_FORECAST;
 let a = 8, b = 6;
 
 [a, b] = [b, a]
+// using destructing array to remove the first two element
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function removeFirstTwo(list) {
+    const [a, b, ...arr] = list;
+    return arr;
+}
+const arr = removeFirstTwo(source);
