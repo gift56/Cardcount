a1b3935d23e478cc160c98293374dff4ba5baec7
gift56 <efeasiughu@gmail.com>
2022-05-14 16:45:52 +0100

    using | indicator

--- myjavscript/regularexp.js
+++ myjavscript/regularexp.js
@@ -2,3 +2,7 @@
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString);
+// using the | sign to mmatch multipule objects
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let results = petRegex.test(petString);
