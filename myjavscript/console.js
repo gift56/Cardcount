576824fed8b47860e57e68f93f3d7570ada15abd
gift56 <efeasiughu@gmail.com>
2022-05-16 18:16:40 +0100

    using of splice method

--- myjavscript/console.js
+++ myjavscript/console.js
@@ -66,3 +66,13 @@ function mixedNumbers(arr) {
 }
 
 console.log(mixedNumbers(['IV', 5, 'six']));
+
+// removing and replacing with splice()
+function htmlColorNames(arr) {
+    // Only change code below this line
+    arr.splice(0, 2, 'DarkSalmon', 'BlanchedAlmond')
+    // Only change code above this line
+    return arr;
+}
+
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
