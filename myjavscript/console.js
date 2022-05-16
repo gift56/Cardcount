9dc7197faf5ef8ad6a640680f378e2191bfd33cc
gift56 <efeasiughu@gmail.com>
2022-05-16 16:15:29 +0100

    fixed unclosed bracket

--- myjavscript/console.js
+++ myjavscript/console.js
@@ -3,3 +3,8 @@ let seven = 7;
 let three = "3";
 console.log(typeof (seven));
 console.log(typeof (three));
+
+// fixing unclosed bracket array and function
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) => previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
