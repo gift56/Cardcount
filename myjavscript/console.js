dc3f4eefd5a3dbff14708780deb8cec3151c5ae5
gift56 <efeasiughu@gmail.com>
2022-05-16 17:07:23 +0100

    fixed nested loop issue

--- myjavscript/console.js
+++ myjavscript/console.js
@@ -35,3 +35,23 @@ let base = 2;
 let exp = 3;
 let power = raiseToPower(base, exp);
 console.log(power);
+
+// correcting nested loop
+function zeroArray(m, n) {
+    // Creates a 2-D array with m rows and n columns of zeroes
+    let newArray = [];
+    for (let i = 0; i < m; i++) {
+        // Adds the m-th row into newArray
+        let row = [];
+        for (let j = 0; j < n; j++) {
+            // Pushes n zeroes into the current row to create the columns
+            row.push(0);
+        }
+        // Pushes the current row, which now has n zeroes in it, to the array
+        newArray.push(row);
+    }
+    return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
