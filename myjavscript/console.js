ec9fb50ca3f6dd58023a1008249bf06849414b26
gift56 <efeasiughu@gmail.com>
2022-05-16 18:59:48 +0100

    using of multi nested array

--- myjavscript/console.js
+++ myjavscript/console.js
@@ -124,3 +124,33 @@ function filteredArray(arr, elem) {
 }
 
 console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+
+// MultiNested array
+let myNestedArray = [
+    // Only change code below this line
+    [
+        "unshift", false, 1, 2, 3, "complex", "nested"
+    ],
+    [
+        "loop", "shift", 6, 7, 1000, "method"
+    ],
+    [
+        "concat", false, true, "spread", "array",
+        ["deep"]
+    ],
+    [
+        "mutate", 1327.98, "splice", "slice", "push",
+        [
+            ["deeper"]
+        ]
+    ],
+    [
+        "iterate", 1.3849, 7, "8.4876", "arbitrary", "depth",
+        [
+            [
+                ["deepest"]
+            ]
+        ]
+    ]
+    // Only change code above this line
+];
