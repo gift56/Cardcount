63e59094fdb6d6b1f172d5557cd8f5b30ad9893c
gift56 <efeasiughu@gmail.com>
2022-05-16 19:26:33 +0100

    using of object

--- myjavscript/console.js
+++ myjavscript/console.js
@@ -154,3 +154,39 @@ let myNestedArray = [
     ]
     // Only change code above this line
 ];
+
+// chececking object if it hasproperties
+let users = {
+    Alan: {
+        age: 27,
+        online: true
+    },
+    Jeff: {
+        age: 32,
+        online: true
+    },
+    Sarah: {
+        age: 48,
+        online: true
+    },
+    Ryan: {
+        age: 19,
+        online: true
+    }
+};
+
+function isEveryoneHere(userObj) {
+    // Only change code below this line
+    if (
+        userObj.hasOwnProperty("Alan") &&
+        userObj.hasOwnProperty("Jeff") &&
+        userObj.hasOwnProperty("Sarah") &&
+        userObj.hasOwnProperty("Ryan")
+    ) {
+        return true;
+    }
+    return false;
+    // Only change code above this line
+}
+
+console.log(isEveryoneHere(users));
