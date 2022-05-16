f9ea49885ec2f2cb83e4358ffe9ade66f463df50
gift56 <efeasiughu@gmail.com>
2022-05-16 16:30:27 +0100

    fixed string error

--- myjavscript/console.js
+++ myjavscript/console.js
@@ -8,3 +8,7 @@ console.log(typeof (three));
 let myArray = [1, 2, 3];
 let arraySum = myArray.reduce((previous, current) => previous + current);
 console.log(`Sum of array values is: ${arraySum}`);
+
+// Using of console to debug for string error
+const textError = "hello my name is gift and i build a website and you can view it on the link below a href='#Home'";
+console.log(textError);
