59d3898610b92a639c879ac8a87f64ef4f0507fa
gift56 <efeasiughu@gmail.com>
2022-05-16 12:47:44 +0100

    using of /w/g

--- myjavscript/regularexp.js
+++ myjavscript/regularexp.js
@@ -60,3 +60,8 @@ let resultT = myText.match(regex);
 let caboose = "The last car on a train is the caboose";
 let lastRegex = /caboose$/; // Change this line
 let resultc = lastRegex.test(caboose);
+
+// Using of shortcourt
+let quoteSampleS = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/g; // Change this line
+let resultS = quoteSample.match(alphabetRegexV2).length;
