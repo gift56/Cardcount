ee82d2b59ba8d8e56e868517cc0bbf0dfd8ac72e
gift56 <efeasiughu@gmail.com>
2022-05-17 10:44:33 +0100

    returning object keys as array

--- myjavscript/console.js
+++ myjavscript/console.js
@@ -217,3 +217,29 @@ function countOnline(usersObj) {
 }
 
 console.log(countOnline(users));
+
+// returning object key as an array using object.key()
+let usersd = {
+    Alan: {
+        age: 27,
+        online: false
+    },
+    Jeff: {
+        age: 32,
+        online: true
+    },
+    Sarah: {
+        age: 48,
+        online: false
+    },
+    Ryan: {
+        age: 19,
+        online: true
+    }
+};
+
+function getArrayOfUsers(obj) {
+    return Object.keys(obj);
+}
+
+console.log(getArrayOfUsers(usersd));
