ff3caa91fec58e8df9bd5b491f6536b644aac6d7
gift56 <efeasiughu@gmail.com>
2022-05-18 13:14:37 +0100

    started learning basic argorithim

---
+++ myjavscript/basicArgorith.js
@@ -0,0 +1,7 @@
+// Using javascript to calculate for celsius and fahrenheit
+function convertToF(celsius) {
+    let fahrenheit = celsius * (9 / 5) + 32;
+    return fahrenheit;
+}
+
+convertToF(30);
