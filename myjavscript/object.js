3bc053f102d687402d14c5b225866ada3630b87a
gift56 <efeasiughu@gmail.com>
2022-05-19 14:38:37 +0100

    binding the chain

--- myjavscript/object.js
+++ myjavscript/object.js
@@ -143,3 +143,15 @@ let beaglep = new Dog("Snoopy");
 
 // Only change code below this line
 Dog.prototype.isPrototypeOf(beagle);
+
+// conbining the chin
+function Dog(name) {
+    this.name = name;
+}
+
+let beagleC = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);  // yields true
+
+// Fix the code below so that it evaluates to true
+Object.prototype.isPrototypeOf(Dog.prototype);
