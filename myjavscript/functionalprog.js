acae284d7949614a6a44dbaeebbdc5e1be6eb43f
gift56 <efeasiughu@gmail.com>
2022-05-19 16:12:43 +0100

    preparing two types of teas for different people

--- myjavscript/functionalprog.js
+++ myjavscript/functionalprog.js
@@ -1,13 +1,40 @@
 // Loop in through to get the actuall number of worker drinking green tea
 const prepareTea = () => 'greenTea';
 const getTea = (numOfCups) => {
-  const teaCups = [];
+    const teaCups = [];
 
-  for(let cups = 1; cups <= numOfCups; cups += 1) {
-    const teaCup = prepareTea();
-    teaCups.push(teaCup);
-  }
-  return teaCups;
+    for (let cups = 1; cups <= numOfCups; cups += 1) {
+        const teaCup = prepareTea();
+        teaCups.push(teaCup);
+    }
+    return teaCups;
 };
 
 const tea4TeamFCC = getTea(40);
+
+// functional terminology
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+
+const getTeas = (prepareTea, numOfCups) => {
+    const teaCups = [];
+
+    for (let cups = 1; cups <= numOfCups; cups += 1) {
+        const teaCup = prepareTea();
+        teaCups.push(teaCup);
+    }
+    return teaCups;
+};
+
+// Only change code below this line
+const tea4GreenTeamFCC = getTeas(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTeas(prepareBlackTea, 13);
+// Only change code above this line
+
+console.log(
+    tea4GreenTeamFCC,
+    tea4BlackTeamFCC
+);
