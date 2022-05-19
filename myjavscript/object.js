f270784ffe7440a875acaac5eb817658382f012d
gift56 <efeasiughu@gmail.com>
2022-05-19 15:09:02 +0100

    making dog have two properties

--- myjavscript/object.js
+++ myjavscript/object.js
@@ -229,3 +229,20 @@ Dog.prototype.constructor = Dog;
 
 let ducks = new Bird();
 let beaglePa = new Dog();
+
+// Making Dog have two properties
+function Animal() { }
+Animal.prototype.eat = function () { console.log("nom nom nom"); };
+
+function Dog() { }
+
+// Only change code below this line
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+Dog.prototype.bark = function () {
+    console.log("Woof!");
+}
+
+// Only change code above this line
+
+let beagleDg = new Dog();
