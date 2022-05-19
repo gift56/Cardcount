b4701f5d7f78f47e9d5159b4df81ade2450f82ec
gift56 <efeasiughu@gmail.com>
2022-05-19 14:44:19 +0100

    adding RY

--- myjavscript/object.js
+++ myjavscript/object.js
@@ -155,3 +155,29 @@ Dog.prototype.isPrototypeOf(beagle);  // yields true
 
 // Fix the code below so that it evaluates to true
 Object.prototype.isPrototypeOf(Dog.prototype);
+
+// creating a supertype objectin order to avoid repeatation {DRY}
+function Cat(name) {
+    this.name = name;
+}
+
+Cat.prototype = {
+    constructor: Cat,
+};
+
+function Bear(name) {
+    this.name = name;
+}
+
+Bear.prototype = {
+    constructor: Bear,
+};
+
+function Animal() { }
+
+Animal.prototype = {
+    constructor: Animal,
+    eat: function () {
+        console.log("nom nom nom")
+    }
+};
