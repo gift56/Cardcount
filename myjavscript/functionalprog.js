3da5c8552b820f7b79093215312877b09eaec691
gift56 <efeasiughu@gmail.com>
2022-05-19 17:23:44 +0100

    using foreach or for loop

--- myjavscript/functionalprog.js
+++ myjavscript/functionalprog.js
@@ -281,3 +281,27 @@ const ratings = watchList.map(list => ({
 // Only change code above this line
 
 console.log(JSON.stringify(ratings));
+
+// Using for loop in iterating through
+// The global variable
+const s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function (callback) {
+    const newArray = [];
+    // Only change code below this line
+    for (let i = 0; i < this.length; i++) {
+        newArray.push(callback(this[i]));
+    }
+
+    /*
+    // or use forEach
+    this.forEach(a => newArray.push(callback(a)));
+     */
+    
+    // Only change code above this line
+    return newArray;
+};
+
+const new_s = s.myMap(function (item) {
+    return item * 2;
+});
