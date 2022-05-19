3e878ac10fb168834bcecc15273d87bcf3b5e033
gift56 <efeasiughu@gmail.com>
2022-05-19 15:16:48 +0100

    overiding a given statement

--- myjavscript/object.js
+++ myjavscript/object.js
@@ -246,3 +246,21 @@ Dog.prototype.bark = function () {
 // Only change code above this line
 
 let beagleDg = new Dog();
+
+// overriding a given construtor functor word
+function Bird() { }
+
+Bird.prototype.fly = function () { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+// Only change code below this line
+Penguin.prototype.fly = function () {
+    return "Alas, this is a flightless bird.";
+}
+// Only change code above this line
+
+let penguin = new Penguin();
+console.log(penguin.fly());
