0af2cb4990970c1b71e089bc63a3010d39c2f16a
gift56 <efeasiughu@gmail.com>
2022-05-19 13:35:38 +0100

    using this keyword in object

---
+++ myjavscript/object.js
@@ -0,0 +1,8 @@
+// Using this keyword on declared object to avoid error
+let dog = {
+    name: "Spot",
+    numLegs: 4,
+    sayLegs: function () { return "This dog has " + this.numLegs + " legs."; }
+};
+
+dog.sayLegs();
