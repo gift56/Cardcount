6f3f6a9aaece755bac886064f8bac7335fa560e1
gift56 <efeasiughu@gmail.com>
2022-05-20 18:52:22 +0100

    fourth question solved

--- myjavscript/lastData.js
+++ myjavscript/lastData.js
@@ -45,3 +45,12 @@ function rot13(str) {
 }
 
 rot13("SERR PBQR PNZC");
+
+// Validating phone numbers
+let regex = /^([+]?1[\s]?)?((?:[(](?:[2-9]1[02-9]|[2-9][02-8][0-9])[)][\s]?)|(?:(?:[2-9]1[02-9]|[2-9][02-8][0-9])[\s.-]?)){1}([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2}[\s.-]?){1}([0-9]{4}){1}$/;
+
+function telephoneCheck(str) {
+    return regex.test(str);
+}
+
+telephoneCheck("555-555-5555");
