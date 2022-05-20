f0473d0be763ea2818b6e325f60824b76a86db05
gift56 <efeasiughu@gmail.com>
2022-05-20 16:44:17 +0100

    searching and replacing strings

--- myjavscript/intimidate.js
+++ myjavscript/intimidate.js
@@ -74,7 +74,7 @@ function spinalCase(str) {
 
 spinalCase('This Is Spinal Tap');
 
-// Pig Latin
+// Pig Latin use to checke if vowel or consonant
 function translatePigLatin(str) {
     // Create variables to be used
     var pigLatin = "";
@@ -101,3 +101,20 @@ function translatePigLatin(str) {
 }
 
 translatePigLatin("consonant");
+
+// Searching and replacing string to upercase or lowercase
+
+function myReplace(str, before, after) {
+    // Check if first character of argument "before" is a capital or lowercase letter and change the first character of argument "after" to match the case
+    if (/^[A-Z]/.test(before)) {
+        after = after[0].toUpperCase() + after.substring(1)
+    }
+    else {
+        after = after[0].toLowerCase() + after.substring(1)
+    }
+
+    // return string with argument "before" replaced by argument "after" (with correct case)
+    return str.replace(before, after);
+}
+
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
