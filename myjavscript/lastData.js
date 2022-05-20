01e0f001948531b69264bb8323e7b33e1d45698b
gift56 <efeasiughu@gmail.com>
2022-05-20 18:32:53 +0100

    converting roman figures

--- myjavscript/lastData.js
+++ myjavscript/lastData.js
@@ -6,3 +6,19 @@ function palindrome(str) {
 
 palindrome("eye");
 palindrome("Racecar");
+
+// second question converting number to roman figure
+function convertToRoman(num) {
+    const numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const numeral = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    const arr = [];
+    let renum = num;
+    while (arr.reduce((before, after) => before + after, 0) < num) {
+        const EL = numbers.findIndex(element => renum - element >= 0);
+        arr.push(numbers[EL]);
+        renum -= numbers[EL];
+    }
+    return arr.map(element => numeral[numbers.indexOf(element)]).join('');
+}
+
+convertToRoman(36);
