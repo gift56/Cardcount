1c58b3530ab1f2c886729b5e9c58b08060819609
gift56 <efeasiughu@gmail.com>
2022-05-20 16:35:26 +0100

    spinal tab close

--- myjavscript/intimidate.js
+++ myjavscript/intimidate.js
@@ -58,3 +58,18 @@ whatIsInAName(
     ],
     { last: "Capulet" }
 );
+
+// Spinal tab using regex
+
+function spinalCase(str) {
+    // Create a variable for the white space and underscores.
+    var regex = /\s+|_+/g;
+
+    // Replace low-upper case to low-space-uppercase
+    str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+
+    // Replace space and underscore with -
+    return str.replace(regex, "-").toLowerCase();
+}
+
+spinalCase('This Is Spinal Tap');
