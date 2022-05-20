274d8b1d0a4f4b0576d16ad57c3af9cfc32d4011
gift56 <efeasiughu@gmail.com>
2022-05-20 16:57:50 +0100

    Using spread method in creating new arrays

--- myjavscript/intimidate.js
+++ myjavscript/intimidate.js
@@ -187,3 +187,19 @@ function fearNotLetter(str) {
 }
 
 fearNotLetter("abce");
+
+// Sorted union
+function uniteUnique(arr) {
+    const args = [...arguments];
+    const result = [];
+    for (let i = 0; i < args.length; i++) {
+        for (let j = 0; j < args[i].length; j++) {
+            if (!result.includes(args[i][j])) {
+                result.push(args[i][j]);
+            }
+        }
+    }
+    return result;
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
