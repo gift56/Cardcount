8e0c0b0016b21f6a39960c22154d723739fba0b8
gift56 <efeasiughu@gmail.com>
2022-05-20 15:02:25 +0100

    using only three method in find on right number

--- myjavscript/functionalprog.js
+++ myjavscript/functionalprog.js
@@ -629,3 +629,13 @@ function getRating(watchList) {
 }
 
 console.log(getRating(watchListF));
+
+// Using onl map(),filter() amd redue() in finding only positive value
+const squareList = arr => {
+    // Only change code below this line
+    return arr.filter(num => num > 0 && num % parseInt(num) === 0).map(num => Math.pow(num, 2));
+    // Only change code above this line
+};
+
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);
