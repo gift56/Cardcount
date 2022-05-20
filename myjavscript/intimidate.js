ffbbf17a10c85d36e935941d1df2f27596c611cd
gift56 <efeasiughu@gmail.com>
2022-05-20 17:25:53 +0100

    steamroller

--- myjavscript/intimidate.js
+++ myjavscript/intimidate.js
@@ -285,3 +285,14 @@ function dropElements(arr, func) {
 }
 
 dropElements([1, 2, 3], function (n) { return n < 3; });
+
+// Steamroller
+function steamrollArray(val, flatArr = []) {
+    val.forEach(item => {
+        if (Array.isArray(item)) steamrollArray(item, flatArr);
+        else flatArr.push(item);
+    });
+    return flatArr;
+}
+
+steamrollArray([1, [2], [3, [[4]]]]);
