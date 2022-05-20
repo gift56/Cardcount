4cc6fcc559417d7327b1e8d30b0de7231cf6e005
gift56 <efeasiughu@gmail.com>
2022-05-20 16:33:10 +0100

    who art thou

--- myjavscript/intimidate.js
+++ myjavscript/intimidate.js
@@ -29,3 +29,32 @@ function destroyer(arr) {
 }
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+//  WHo art Thou in javascript
+function whatIsInAName(collection, source) {
+
+    // Only change code below this line
+    const souceKeys = Object.keys(source);
+
+    // filter the collection
+    return collection.filter(obj => {
+        for (let i = 0; i < souceKeys.length; i++) {
+            if (!obj.hasOwnProperty(souceKeys[i]) ||
+                obj[souceKeys[i]] !== source[souceKeys[i]]) {
+                return false;
+            }
+        }
+        return true;
+    });
+
+    // Only change code above this line
+}
+
+whatIsInAName(
+    [
+        { first: "Romeo", last: "Montague" },
+        { first: "Mercutio", last: null },
+        { first: "Tybalt", last: "Capulet" }
+    ],
+    { last: "Capulet" }
+);
