937ec65ea2a741578b65535627f7d453c2229ace
gift56 <efeasiughu@gmail.com>
2022-05-20 17:18:33 +0100

    dropping it

--- myjavscript/intimidate.js
+++ myjavscript/intimidate.js
@@ -270,3 +270,18 @@ function smallestCommons(arr) {
 }
 
 smallestCommons([1, 5]);
+
+// Dropping it 
+function dropElements(arr, func) {
+    let originalLen = arr.length;
+    for (let i = 0; i < originalLen; i++) {
+        if (func(arr[0])) {
+            break;
+        } else {
+            arr.shift();
+        }
+    }
+    return arr;
+}
+
+dropElements([1, 2, 3], function (n) { return n < 3; });
