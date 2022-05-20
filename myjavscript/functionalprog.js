c9a34b2b7dfc827c1ff76b2a069a83d075f2425a
gift56 <efeasiughu@gmail.com>
2022-05-20 14:38:18 +0100

    combing two arrays

--- myjavscript/functionalprog.js
+++ myjavscript/functionalprog.js
@@ -471,3 +471,15 @@ function sliceArray(anim, beginSlice, endSlice) {
 
 const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
 sliceArray(inputAnim, 1, 3);
+
+// combining two array
+function nonMutatingConcat(original, attach) {
+    // Only change code below this line
+
+    return original.concat(attach);
+    // Only change code above this line
+}
+
+const first = [1, 2, 3];
+const second = [4, 5];
+nonMutatingConcat(first, second);
